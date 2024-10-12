57f03106318aca1003c780eb3ab39317b0bed63d
Fadi Hanna <fadialias230@gmail.com>
2024-10-12 23:18:47 +0200

    Update right tsconfig rules to build project, ignore Dist folder in stylelintrc rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -17,7 +17,7 @@ const compat = new FlatCompat({
 
 export default [
   {
-    ignores: ["**/dist", "**/.eslintrc.cjs"],
+    ignores: ["dist/**", "**/.eslintrc.cjs"],
   },
   ...fixupConfigRules(
     compat.extends(
