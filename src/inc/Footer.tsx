046f3e030f0f448ceb99829a130889776cb573b7
Fadi Hanna <fadialias230@gmail.com>
2024-10-26 23:21:43 +0200

    Add download my cv icon

--- src/inc/Footer.tsx
+++ src/inc/Footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <div className="text-center p-2">
       <a href="Fadi_Hanna_CV_IT.pdf" download="Fadi_Hanna_CV_IT.pdf">
-        Download My CV
+        <i className="fa-solid fa-file m-2"></i> Download My CV
       </a>
     </div>
   );
