46a588346c06cceeb467b205c7fba9a7f4a9cb4e
Fadi Hanna <fadialias230@gmail.com>
2024-12-20 21:26:04 +0100

    Updates

---
+++ _next/static/chunks/app/layout-bd5315ca2a0b92b9.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{6396:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,7143,23))},7143:()=>{}},e=>{var s=s=>e(e.s=s);e.O(0,[230,441,517,358],()=>s(6396)),_N_E=e.O()}]);
