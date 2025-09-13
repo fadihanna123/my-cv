1c92af07ebe919efbce7cd965a676e412494f17b
fadihanna123 <fadialias230@gmail.com>
2025-09-13 03:40:18 +0200

    Fix white background above the container

--- app/page.tsx
+++ app/page.tsx
@@ -18,30 +18,28 @@ const Layout: React.FC = () => {
   return (
     <MyGlobalContext.Provider value={{ darkMode, setDarkMode }}>
       <div
-        className={`wrapper container m-0 overflow-auto border-0 p-0 text-lg font-bold text-black outline-0 ${
+        className={`wrapper container m-0 overflow-auto border-0 p-0 text-lg font-bold text-black outline-0 bg-neatural-100 dark:bg-neutral-900 ${
           !darkMode && "dark"
         }`}
       >
-        <div className="bg-neatural-100 dark:bg-neutral-900">
-          <Header />
-          <div className="border-r-2 border-solid border-black p-2">
-            <PersonCardSection />
-          </div>
-          <div>
-            <SkillsSection />
-          </div>
-          <div className="ml-0 border-r-2 border-solid border-black p-2 sm:ml-20">
-            <div id="experience">
-              <ExperienceSection />
-              <EducationSection />
-            </div>
-            <div id="projects">
-              <ProjectsSection />
-            </div>
-            <ContactSection />
-          </div>
-          <Footer />
+        <Header />
+        <div className="border-r-2 border-solid border-black p-2">
+          <PersonCardSection />
         </div>
+        <div>
+          <SkillsSection />
+        </div>
+        <div className="ml-0 border-r-2 border-solid border-black p-2 sm:ml-20">
+          <div id="experience">
+            <ExperienceSection />
+            <EducationSection />
+          </div>
+          <div id="projects">
+            <ProjectsSection />
+          </div>
+          <ContactSection />
+        </div>
+        <Footer />
       </div>
     </MyGlobalContext.Provider>
   );
